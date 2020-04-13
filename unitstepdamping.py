10213e586c7f9f7525febec6360775c483f453c2
Sivanidevarapalli26 <47571962+Sivanidevarapalli26@users.noreply.github.com>
2020-04-13 15:42:43 +0530

    Update unitstepdamping.py

--- unitstepdamping.py
+++ unitstepdamping.py
@@ -3,13 +3,15 @@ import math
 import matplotlib.pyplot as plt
 a=float(input("Enter a value="))
 b=float(input("Enter b value="))
+Y=np.array([])
 def u(n):
     if(n>=0):
         return 1
     else:
         return 0
 t=np.linspace(0,10,1000)
-
-y=((np.exp(-a*t)*np.sin(math.sqrt((b-(a**2/4))*t))*u(t)))/(math.sqrt(b-(a**2/4)))
-plt.plot(t,y)
+for i in t:
+    y=((np.exp(-a*i)*np.sin(math.sqrt((b-(a**2/4))*i))*u(i)))/(math.sqrt(b-(a**2/4)))
+    Y= np.append(Y,[y])
+plt.plot(t,Y)
 plt.show()
